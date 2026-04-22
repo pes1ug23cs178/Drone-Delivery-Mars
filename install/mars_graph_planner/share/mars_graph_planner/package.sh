f542e30300e5b029aa82206d371ff045c3c7c9d5
Shihab K A <shibukarattil2004@gmail.com>
2026-04-22 22:39:27 +0530

    Updation

--- install/mars_graph_planner/share/mars_graph_planner/package.sh
+++ install/mars_graph_planner/share/mars_graph_planner/package.sh
@@ -80,6 +80,7 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/mars_graph_planner/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/mars_graph_planner/hook/ament_prefix_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/mars_graph_planner/share/mars_graph_planner/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
